d223d05b643d1e3d76f64817697b278d4315bbde
Velqein <rtete05@gmail.com>
2026-05-14 00:23:35 +0530

    chore: clear all files (backup retained locally)

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// base: '/velqein.com/' → GitHub Pages project URL (velqein.github.io/velqein.com/)
-// Change to '/' if you add a custom domain in Pages settings
-export default defineConfig({
-  plugins: [react()],
-  base: '/velqein.com/',
-})
